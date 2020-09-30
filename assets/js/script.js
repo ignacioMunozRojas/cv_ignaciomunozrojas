171f7e461a575cf4cde0dfa32c60063e5168cbe5
Ignacio <nacho_mr90@hotmail.com>
2020-09-30 13:54:54 -0300

    Sexta subida - Sección sobre mi + cambio de imagen portada + Links footer RRSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,16 @@
 $(function() {
 
-    $('a[href*="#"]')
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-    if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-        && 
-        location.hostname == this.hostname
-    ) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 600, function() {
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { 
-            return false;
-            } else {
-            $target.attr('tabindex','-1');
-            $target.focus();
-            };
-        });
+    $("a").click(function(event){
+        if (this.hash != "") {
+            event.preventDefault();
+            var gato = this.hash;
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function(){
+                window.location.hash = gato;
+            });
         }
-    }
     });
 
-    
     $('[data-toggle="tooltip"]').tooltip();
 })
